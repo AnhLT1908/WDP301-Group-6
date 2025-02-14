f90cd634d8d74839d0ef38e2147b58a22c1403fa
Le Tien Anh <167272330+AnhLT1908@users.noreply.github.com>
2025-02-15 03:40:26 +0700

    HaiTH create layout login, change password successfully (#11)
    * Update gitignore
    
    * Install tailwindcss to project. Create bass Login, ChangePassword. Have to fix UI Login, ChangePassword later.
    
    * change layout login, change password
    
    * edit project structure
    
    * change in structure
    
    * delete unused folder
    
    * resolve conflic
    
    ---------
    
    Co-authored-by: HaiTran0303 <haiprotpp@gmail.com>

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,11 +1,12 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {
-      clipPath: {
-        trapezoid: "polygon(10% 0, 100% 0, 90% 100%, 0 100%)",
+ /** @type {import('tailwindcss').Config} */
+ export default {
+    content: ["./src/**/*.{html,jsx,js}"],
+    theme: {
+      extend: {
+        clipPath: {
+          trapezoid: "polygon(10% 0, 100% 0, 90% 100%, 0 100%)",
+        },
       },
     },
-  },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
+    plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  }
