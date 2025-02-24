05d2832ac45989ed2cd8fff965eb5a1730f8f742
HongNT03 <nguyentienhong220903@gmail.com>
2025-02-24 09:30:24 +0700

    config develop branch

--- be/config/connectDB.js
+++ be/config/connectDB.js
@@ -1,16 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const ConnectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log(`✅ MongoDB connected: ${process.env.MONGODB_URL}`);
-    } catch (err) {
-        console.error("❌ MongoDB connection error:", err);
-        process.exit(1); // Dừng server nếu kết nối thất bại
-    }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: process.env.DB_NAME,
+    });
+    console.log(`✅ MongoDB connected: ${mongoose.connection.host}`);
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1); // Exit the process on failure
+  }
 };
 
 export default ConnectDB;
