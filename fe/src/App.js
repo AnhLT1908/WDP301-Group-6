f81b9f11dc824d74036a02e57d6237880084b59c
Trần Hoàng Hải <109613254+HaiTran0303@users.noreply.github.com>
2025-02-24 09:33:35 +0700

    Feature/#10 dashboard (#16)
    * Install tailwindcss to project. Create bass Login, ChangePassword. Have to fix UI Login, ChangePassword later.
    
    * change layout login, change password
    
    * edit project structure
    
    * change in structure
    
    * delete unused folder
    
    * resolve conflic
    
    * complete fix UI Login Page, UI Change Password Page
    
    * Doing Dashboard Header, Dashboard Sidebar
    
    * On doing Dashboard Layout
    
    * On doing User Profile
    
    * Create Account List
    
    * fix DashboardHeader
    
    * fix DashboardSideBar, AccountList
    
    * complete base admin dashboard, account list
    
    * complete base form user profile
    
    ---------
    
    Co-authored-by: Le Tien Anh <167272330+AnhLT1908@users.noreply.github.com>

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,24 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ForgotPassword from "./page/ForgotPassword.jsx";
-import VerifyCodeForgotPassword from "./page/VerifyCodeForgotPassword.jsx";
-import HomePage from "./page/HomePage.jsx";
-import Login from "./components/Login";
-import ChangePassword from "./components/ChangePassword";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DashboardLayout from './components/layout/DashboardLayout';
+import ForgotPassword from './page/ForgotPassword.jsx';
+import VerifyCodeForgotPassword from './page/VerifyCodeForgotPassword.jsx';
+import HomePage from './page/HomePage.jsx';
+import Login from './page/Login.jsx';
+import ChangePassword from './page/ChangePassword.jsx';
+import AccountList from './page/AccountList';
+import UserProfile from './page/UserProfile';
+
+function Dashboard() {
+  return <div>Welcome to Dashboard</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/verify-code" element={<VerifyCodeForgotPassword />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/change-password" element={<ChangePassword />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/admin"
+          element={<DashboardLayout><Dashboard /></DashboardLayout>} />
+        <Route
+          path="/admin/account-list"
+          element={<DashboardLayout><AccountList /></DashboardLayout>} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/verify-code" element={<VerifyCodeForgotPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/change-password" element={<ChangePassword />} />
+      </Routes>
+    </Router>
   );
 }
 
