a2640823440b3bc695e063c5be1c875c27944139
HongNT03 <nguyentienhong220903@gmail.com>
2025-02-27 08:35:14 +0700

    MANAGE EQUIIPMENT

--- be/router/IndexRoute.js
+++ be/router/IndexRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import AccountRouter from '../router/AccountRoute.js';
 import AuthRouter from '../router/AuthRouter.js';
+import RoomRouter from './RoomRoute.js';
 
 const indexRouter = express.Router();
 
 indexRouter.use("/account", AccountRouter);
 indexRouter.use("/auth", AuthRouter);
+indexRouter.use("/room", RoomRouter);
 
 export default indexRouter;
