73d9c04b4053cfeb5bd3e2a16b3154742450be64
Le Tien Anh <anhlt1908.work@gmail.com>
2025-03-05 22:57:24 +0700

    UC1-Login completed

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import DashboardLayout from "./components/layout/DashboardLayout";
 import ForgotPassword from "./page/ForgotPassword.jsx";
 import VerifyCodeForgotPassword from "./page/VerifyCodeForgotPassword.jsx";
@@ -9,47 +10,82 @@ import AccountList from "./page/AccountList";
 import UserProfile from "./page/UserProfile";
 import SetNewPassword from "./page/SetNewPassword.jsx";
 import HomePageAdmin from "./page/HomePageAdmin.jsx";
-
+//import NotFound from "./page/NotFound.jsx";
 
 function Dashboard() {
   return <div>Welcome to Dashboard</div>;
 }
 
 function App() {
+  const [accountType, setAccountType] = useState(null);
+
+  useEffect(() => {
+    const storedAccountType = localStorage.getItem("accountType");
+    setAccountType(storedAccountType);
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/admin"
-          element={
-            <DashboardLayout>
-              <HomePageAdmin />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/admin/dashboard"
-          element={
-            <DashboardLayout>
-              <Dashboard />
-            </DashboardLayout>
-          }
-        />
-        <Route
-          path="/admin/account-list"
-          element={
-            <DashboardLayout>
-              <AccountList />
-            </DashboardLayout>
-          }
-        />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        <Route path="/login" element={<Login setAccountType={setAccountType} />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/user-profile" element={<UserProfile />} />
         <Route path="/verify-code" element={<VerifyCodeForgotPassword />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/change-password" element={<ChangePassword />} />
         <Route path="/reset-password" element={<SetNewPassword />} />
+
+        {accountType === "Lodger" ? (
+          <Route path="/home" element={<HomePage />} />
+        ) : (
+          <Route path="/home" element={<Navigate to="/login" replace />} />
+        )}
+
+        {accountType === "Admin" ? (
+          <>
+            <Route
+              path="/admin"
+              element={
+                <DashboardLayout>
+                  <HomePageAdmin />
+                </DashboardLayout>
+              }
+            />
+            <Route
+            path="/admin/dashboard"
+            element={
+              <DashboardLayout>
+                <Dashboard />
+              </DashboardLayout>
+            }
+            />
+            <Route
+              path="/admin/account-list"
+              element={
+                <DashboardLayout>
+                  <AccountList />
+                </DashboardLayout>
+              }
+            />
+          </>
+        ) : (
+          <Route path="/admin/*" element={<Navigate to="/login" replace />} />
+        )}
+
+        {accountType === "Manager" ? (
+          <Route
+            path="/manager"
+            element={
+              ""
+            }
+          />
+        ) : (
+          <Route path="/manager/*" element={<Navigate to="/login" replace />} />
+        )}
+
+        <Route path="/404" element={""} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
     </Router>
   );
