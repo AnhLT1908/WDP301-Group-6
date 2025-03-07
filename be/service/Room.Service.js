a92a297d989399c6cb9bf6458f93525f37350af0
HongNT03 <166291733+HongNT03@users.noreply.github.com>
2025-03-07 11:56:49 +0700

    finish hongnt api backend (#33)
    Co-authored-by: Le Tien Anh <167272330+AnhLT1908@users.noreply.github.com>

--- be/service/Room.Service.js
+++ be/service/Room.Service.js
@@ -1,26 +1,46 @@
 // Room.Service.js
 import DefaultUtilities from "../model/DefaultUtilities.js";
 import Room from "../model/Room.js";
-import exceljs from 'exceljs';
-import bcrypt from 'bcrypt';
+import exceljs from "exceljs";
+import bcrypt from "bcrypt";
 import Account from "../model/Account.js";
 import Bills from "../model/Bills.js";
 import House from "../model/House.js";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-export const getAllRoom = async(req, res, next)=>{
+export const ViewListEquipment = async (req, res, next) => {
+  try {
+    const listroom = await Room.find().populate("utilities");
+    const filteredUtility = listroom.map((room) => ({
+      roomId: room._id,
+      roomName: room.name,
+      utilities: room.utilities.map((utility) => utility.name), // Extract only utility names
+    }));
+    if (!listroom || listroom.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No utilities found in system",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: filteredUtility,
+    });
+  } catch (error) {}
+};
+
+export const getAllRoom = async (req, res, next) => {
   try {
     const rooms = await Room.find();
     res.status(200).json({
       success: true,
       count: rooms.length,
       data: rooms,
-  });
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
-
+};
 
 export const ViewListUtilities = async (req, res) => {
   try {
@@ -209,86 +229,102 @@ export const DeleteUtilities = async (req, res) => {
 };
 
 export const getRoomEquipment = async (req, res) => {
-    try {
-        const { roomId } = req.params;
+  try {
+    const { roomId } = req.params;
 
-        const room = await Room.findById(roomId)
-            .populate("utilities")
-            .populate("otherUtilities");
+    const room = await Room.findById(roomId)
+      .populate("utilities")
+      .populate("otherUtilities");
 
-        if (!room) {
-            return res.status(404).json({ message: "Room not found" });
-        }
-
-        res.json({
-            roomId: room._id,
-            name: room.name,
-            equipment: [...room.utilities, ...room.otherUtilities]
-        });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Server error" });
+    if (!room) {
+      return res.status(404).json({ message: "Room not found" });
     }
+
+    res.json({
+      roomId: room._id,
+      name: room.name,
+      equipment: [...room.utilities, ...room.otherUtilities],
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
 
 export const addRoom = async (req, res, next) => {
   try {
-      const { houseId, name, floor, status, quantityMember, roomType, roomPrice, deposit, area } = req.body;
+    const {
+      houseId,
+      name,
+      floor,
+      status,
+      quantityMember,
+      roomType,
+      roomPrice,
+      deposit,
+      area,
+    } = req.body;
 
-      if (!houseId || !name || !roomPrice || !quantityMember || !area ) {
-          return res.status(400).json({
-              success: false,
-              message: "Thiếu thông tin bắt buộc! (houseId, name, roomPrice, quantityMember, area, email)",
-          });
-      }
-
-      const house = await House.findById(houseId);
-      if (!house) {
-          return res.status(404).json({ success: false, message: "House không tồn tại!" });
-      }
-
-      const existingRoom = await Room.findOne({ houseId, name });
-      if (existingRoom) {
-          return res.status(400).json({ success: false, message: `Phòng '${name}' đã tồn tại.` });
-      }
-
-      const newRoom = await Room.create({
-          floor: floor || name.charAt(0),
-          name,
-          status: status || "Empty",
-          quantityMember,
-          roomType: roomType || "normal",
-          roomPrice,
-          deposit: deposit || 0,
-          area,
-          houseId,
-          utilities: house.utilities || [],
-          otherUtilities: house.otherUtilities || [],
-          deleted: false,
-          deletedAt: null,
+    if (!houseId || !name || !roomPrice || !quantityMember || !area) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Thiếu thông tin bắt buộc! (houseId, name, roomPrice, quantityMember, area, email)",
       });
+    }
 
-      house.numberOfRoom += 1;
-      await house.save();
+    const house = await House.findById(houseId);
+    if (!house) {
+      return res
+        .status(404)
+        .json({ success: false, message: "House không tồn tại!" });
+    }
 
-      return res.status(201).json({
-          success: true,
-          message: "Tạo phòng thành công!",
-          data: newRoom,
-      });
+    const existingRoom = await Room.findOne({ houseId, name });
+    if (existingRoom) {
+      return res
+        .status(400)
+        .json({ success: false, message: `Phòng '${name}' đã tồn tại.` });
+    }
+
+    const newRoom = await Room.create({
+      floor: floor || name.charAt(0),
+      name,
+      status: status || "Empty",
+      quantityMember,
+      roomType: roomType || "normal",
+      roomPrice,
+      deposit: deposit || 0,
+      area,
+      houseId,
+      utilities: house.utilities || [],
+      otherUtilities: house.otherUtilities || [],
+      deleted: false,
+      deletedAt: null,
+    });
+
+    house.numberOfRoom += 1;
+    await house.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Tạo phòng thành công!",
+      data: newRoom,
+    });
   } catch (error) {
-      next(error);
+    next(error);
   }
 };
 
-export const GetOne = async (req, res, next) => {
+export const getRoomDetail = async (req, res, next) => {
   try {
     const { roomId } = req.params;
 
     // Kiểm tra roomId có hợp lệ không
     if (!mongoose.Types.ObjectId.isValid(roomId)) {
-      return res.status(400).json({ success: false, message: "Invalid roomId" });
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid roomId" });
     }
 
     const room = await Room.findById(roomId)
@@ -301,7 +337,9 @@ export const GetOne = async (req, res, next) => {
 
     // Nếu không tìm thấy phòng
     if (!room) {
-      return res.status(404).json({ success: false, message: "Room not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Room not found" });
     }
 
     // Kiểm tra nếu `members` tồn tại trước khi truy cập `.length`
@@ -321,85 +359,82 @@ export const GetOne = async (req, res, next) => {
 };
 
 export const getRoomServices = async (req, res, next) => {
-    try {
-        const { roomId } = req.params;
+  try {
+    const { roomId } = req.params;
 
-        // Lấy thông tin phòng
-        const room = await Room.findById(roomId)
-            .populate("utilities")
+    // Lấy thông tin phòng
+    const room = await Room.findById(roomId).populate("utilities");
 
-        if (!room) {
-            return res.status(404).json({ message: "Không tìm thấy phòng!" });
-        }
-
-        // Lấy hóa đơn mới nhất của phòng
-        const latestBill = await Bills.findOne({ roomId })
-            .sort({ createdAt: -1 }) // Lấy hóa đơn mới nhất
-            .limit(1);
-
-        // Tạo danh sách phí dịch vụ
-        const services = [];
-
-        // Thêm tiền phòng
-        services.push({
-            name: "Tiền thuê phòng",
-            price: room.roomPrice,
-        });
-
-        // Thêm các tiện ích mặc định
-        if (room.utilities.length > 0) {
-            room.utilities.forEach((utility) => {
-                services.push({
-                    name: utility.name,
-                    price: utility.price,
-                });
-            });
-        }
-
-        // Thêm các tiện ích khác
-        if (room.otherUtilities.length > 0) {
-            room.otherUtilities.forEach((utility) => {
-                services.push({
-                    name: utility.name,
-                    price: utility.price,
-                });
-            });
-        }
-
-        // Nếu có hóa đơn mới nhất, lấy tổng số tiền cần thanh toán
-        let totalAmount = room.roomPrice;
-        if (latestBill) {
-            totalAmount = latestBill.total;
-        }
-
-        res.json({
-            room: room.name,
-            services,
-            totalAmount,
-        });
-    } catch (error) {
-        next(error);
+    if (!room) {
+      return res.status(404).json({ message: "Không tìm thấy phòng!" });
     }
+
+    // Lấy hóa đơn mới nhất của phòng
+    const latestBill = await Bills.findOne({ roomId })
+      .sort({ createdAt: -1 }) // Lấy hóa đơn mới nhất
+      .limit(1);
+
+    // Tạo danh sách phí dịch vụ
+    const services = [];
+
+    // Thêm tiền phòng
+    services.push({
+      name: "Tiền thuê phòng",
+      price: room.roomPrice,
+    });
+
+    // Thêm các tiện ích mặc định
+    if (room.utilities.length > 0) {
+      room.utilities.forEach((utility) => {
+        services.push({
+          name: utility.name,
+          price: utility.price,
+        });
+      });
+    }
+
+    // Thêm các tiện ích khác
+    if (room.otherUtilities.length > 0) {
+      room.otherUtilities.forEach((utility) => {
+        services.push({
+          name: utility.name,
+          price: utility.price,
+        });
+      });
+    }
+
+    // Nếu có hóa đơn mới nhất, lấy tổng số tiền cần thanh toán
+    let totalAmount = room.roomPrice;
+    if (latestBill) {
+      totalAmount = latestBill.total;
+    }
+
+    res.json({
+      room: room.name,
+      services,
+      totalAmount,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-
-export const addMember = async(req, res, next) =>{
+export const addMember = async (req, res, next) => {
   try {
     const { roomId } = req.params;
     const room = await Room.findById(roomId);
 
     if (!room) {
-        throw new Error("Không tìm thấy phòng.");
+      throw new Error("Không tìm thấy phòng.");
     }
 
     const existingMember = room.members.find(
-        (member) =>
-            member.phone === req.body.phone ||
-            member.cccd === req.body.cccd
+      (member) =>
+        member.phone === req.body.phone || member.cccd === req.body.cccd
     );
 
     if (existingMember) {
-        throw new Error("Số điện thoại hoặc số CCCD đã tồn tại trong phòng.");
+      throw new Error("Số điện thoại hoặc số CCCD đã tồn tại trong phòng.");
     }
 
     //  Bỏ qua xử lý hình ảnh
@@ -408,10 +443,9 @@ export const addMember = async(req, res, next) =>{
 
     return room.members[room.members.length - 1]._doc;
   } catch (error) {
-      throw error;
+    throw error;
   }
-}
-
+};
 
 export const ChangeRoomStatus = async (req, res) => {
   const validStatuses = ["Empty", "Full", "Available"];
@@ -444,60 +478,76 @@ export const ChangeRoomStatus = async (req, res) => {
 
 export const updateRoomDetails = async (req, res) => {
   try {
-      const { roomId } = req.params;
-      const updateData = req.body;
+    const { roomId } = req.params;
+    const updateData = req.body;
 
-      // Allowed fields for updating
-      const allowedFields = [
-          "floor",
-          "name",
-          "status",
-          "quantityMember",
-          "roomType",
-          "roomPrice",
-          "deposit",
-          "utilities",
-          "otherUtilities",
-          "area",
-          "houseId",
-          "members",
-          "deleted"
-      ];
+    // Allowed fields for updating
+    const allowedFields = [
+      "floor",
+      "name",
+      "status",
+      "quantityMember",
+      "roomType",
+      "roomPrice",
+      "deposit",
+      "utilities",
+      "otherUtilities",
+      "area",
+      "houseId",
+      "members",
+      "deleted",
+    ];
 
-      // Validate if the provided keys are allowed
-      const updateKeys = Object.keys(updateData);
-      const isValidUpdate = updateKeys.every(key => allowedFields.includes(key));
+    // Validate if the provided keys are allowed
+    const updateKeys = Object.keys(updateData);
+    const isValidUpdate = updateKeys.every((key) =>
+      allowedFields.includes(key)
+    );
 
-      if (!isValidUpdate) {
-          return res.status(400).json({ error: `Invalid update fields: ${updateKeys.join(", ")}` });
+    if (!isValidUpdate) {
+      return res
+        .status(400)
+        .json({ error: `Invalid update fields: ${updateKeys.join(", ")}` });
+    }
+
+    // Validate status if it's being updated
+    if (updateData.status) {
+      const validStatuses = ["Empty", "Full", "Available"];
+      if (!validStatuses.includes(updateData.status)) {
+        return res.status(400).json({
+          error: `Invalid status value. Allowed values: ${validStatuses.join(
+            ", "
+          )}`,
+        });
       }
+    }
 
-      // Validate status if it's being updated
-      if (updateData.status) {
-          const validStatuses = ["Empty", "Full", "Available"];
-          if (!validStatuses.includes(updateData.status)) {
-              return res.status(400).json({ error: `Invalid status value. Allowed values: ${validStatuses.join(", ")}` });
-          }
+    // Validate roomType if it's being updated
+    if (updateData.roomType) {
+      const validRoomTypes = ["normal", "premium"];
+      if (!validRoomTypes.includes(updateData.roomType)) {
+        return res.status(400).json({
+          error: `Invalid room type. Allowed values: ${validRoomTypes.join(
+            ", "
+          )}`,
+        });
       }
+    }
 
-      // Validate roomType if it's being updated
-      if (updateData.roomType) {
-          const validRoomTypes = ["normal", "premium"];
-          if (!validRoomTypes.includes(updateData.roomType)) {
-              return res.status(400).json({ error: `Invalid room type. Allowed values: ${validRoomTypes.join(", ")}` });
-          }
-      }
+    // Update the room details
+    const updatedRoom = await Room.findByIdAndUpdate(roomId, updateData, {
+      new: true,
+    });
 
-      // Update the room details
-      const updatedRoom = await Room.findByIdAndUpdate(roomId, updateData, { new: true });
+    if (!updatedRoom) {
+      return res.status(404).json({ error: "Room not found" });
+    }
 
-      if (!updatedRoom) {
-          return res.status(404).json({ error: "Room not found" });
-      }
-
-      return res.status(200).json({ message: "Room details updated successfully", room: updatedRoom });
+    return res.status(200).json({
+      message: "Room details updated successfully",
+      room: updatedRoom,
+    });
   } catch (error) {
-      return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 };
-
