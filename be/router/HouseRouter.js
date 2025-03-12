34de4d86199c629619fa0b4557b5d80905f2e6bd
Le Tien Anh <167272330+AnhLT1908@users.noreply.github.com>
2025-03-12 21:00:25 +0700

    Update HouseRouter.js

--- be/router/HouseRouter.js
+++ be/router/HouseRouter.js
@@ -3,7 +3,6 @@ import HouseController from '../controller/HouseController.js';
 import { protect } from '../middleware/verifyToken.js';
 const HouseRouter = express.Router();
 
-HouseRouter.post("/", protect,  HouseController.addHouse);
 HouseRouter.get("/fee",  HouseController.viewServiceFee);
 HouseRouter.put("/update-fee/:houseId",HouseController.updateFee)
 HouseRouter.put("/:houseId", protect, HouseController.updateOne);
