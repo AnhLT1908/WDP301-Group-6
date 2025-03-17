ea9206e9bf65bea294f3a6a1cac5db37b3d8c098
Le Tien Anh <167272330+AnhLT1908@users.noreply.github.com>
2025-03-17 09:22:14 +0700

    Update HouseRouter.js

--- be/router/HouseRouter.js
+++ be/router/HouseRouter.js
@@ -8,8 +8,6 @@ HouseRouter.put("/update-fee/:houseId",HouseController.updateFee)
 HouseRouter.put("/:houseId", protect, HouseController.updateOne);
 HouseRouter.get("/houseByHost", protect, HouseController.getHouseByHostId);
 HouseRouter.get("/:houseId", HouseController.getOne);
-//HouseRouter.get("/:houseId", protect, HouseController.getOne);
-HouseRouter.get("/:houseId", HouseController.getOne);
 HouseRouter.put("/:houseId/change-status", protect, HouseController.ChangeHouseStatus);
 HouseRouter.get("/",  HouseController.getAll);
 HouseRouter.post("/create", protect, HouseController.addHouse);
