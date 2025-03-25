7afc16cd1ca93b518018167b200503fd054bfdb2
Le Tien Anh <anhlt1908.work@gmail.com>
2025-03-25 17:33:22 +0700

    Update API Statistic Service and Admin Dashboard UI

--- be/router/StatisticsRoute.js
+++ be/router/StatisticsRoute.js
@@ -1,11 +1,38 @@
-import express from 'express';
-import StatisticsController from '../controller/StatisticsController.js';
-import protect from '../middleware/verifyToken.js';
+import express from "express";
+import StatisticsController from "../controller/StatisticsController.js";
+import protect from "../middleware/verifyToken.js";
 const StatisticRouter = express.Router();
 
-StatisticRouter.get("/general", protect, StatisticsController.statisticGenneral)
-StatisticRouter.get("/bills", protect, StatisticsController.statisticAllBills)
-StatisticRouter.get("/problems", protect, StatisticsController.statisticProblem)
-StatisticRouter.get("/revenue", protect, StatisticsController.statisticRevenue)
-
+StatisticRouter.get(
+  "/general",
+  protect,
+  StatisticsController.statisticGenneral
+);
+StatisticRouter.get("/bills", protect, StatisticsController.statisticAllBills);
+StatisticRouter.get(
+  "/house/bills",
+  protect,
+  StatisticsController.statisticHouselBills
+);
+StatisticRouter.get(
+  "/house/problems",
+  protect,
+  StatisticsController.statisticHouseProblem
+);
+StatisticRouter.get(
+  "/house/revenues",
+  protect,
+  StatisticsController.statisticHouseRevenue
+);
+StatisticRouter.get(
+  "/problems",
+  protect,
+  StatisticsController.statisticProblem
+);
+StatisticRouter.get(
+  "/revenues",
+  protect,
+  StatisticsController.statisticRevenue
+);
+ 
 export default StatisticRouter;
