b5c556606525cec5d1405d0bb97ea20921f3be17
Le Tien Anh <167272330+AnhLT1908@users.noreply.github.com>
2025-03-29 04:14:45 +0700

    Update upload bill evidence and send bill information again (#72)

--- be/router/BillRouter.js
+++ be/router/BillRouter.js
@@ -6,7 +6,7 @@ const BillRouter = express.Router();
 // Tạo hóa đơn cho phòng
 BillRouter.post("/room/:roomId", BillController.addBillinRoom);
 // Xác nhận thanh toán hóa đơn
-BillRouter.put('/confirm/:billId', BillController.autoConfimBill);
+BillRouter.put("/confirm/:billId", BillController.autoConfimBill);
 BillRouter.post("/webhook/casso", BillController.handleWebHook);
 BillRouter.get("/", BillController.getAllBill);
 BillRouter.get("/roomBill/:roomId", BillController.getBillsByRoom);
@@ -16,5 +16,9 @@ BillRouter.patch(
   "/bill-update/:billId",
   BillController.updateBillPaymentStatus
 );
+BillRouter.post("/evidence/:billId", BillController.uploadBillEvidence);
+BillRouter.delete("/evidence/:billId", BillController.deleteBillEvidence);
+BillRouter.get("/evidence/:billId", BillController.getBillEvidence);
+BillRouter.get("/sendBill/:billId", BillController.sendBillInformationAgain);
 
 export default BillRouter;
--- fe/src/page/LodgerAccountList.jsx
+++ fe/src/page/LodgerAccountList.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import authorsTableData from "../data/authors-table-data";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
